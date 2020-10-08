5f544f81500425d99d22b2927fd2377b6405b00a
raimon rojas <raimoncamilo1@hotmail.com>
2020-10-07 22:27:36 -0500

    Proyecto

---
+++ ConsoleApp1Proyect/ConsoleApp1Proyect/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp1Proyect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
